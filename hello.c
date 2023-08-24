ae07e4d77f7483c5b3e148733a6aa27a5cbaff85
Aiman Zandinan <azandinan2@huskers.unl.edu>
2023-08-24 13:39:27 -0500

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Aiman Zandinan
+ * Date: 2022/08/24
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Computer Sci\n");
+
+
+  return 0;
+}
+
